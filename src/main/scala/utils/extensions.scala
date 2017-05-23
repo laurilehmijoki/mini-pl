7d16b82792c1255aa50e32d097d589378f6e9e96
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2017-05-23 14:58:20 +0300

    docs: fix color

--- src/main/scala/utils/extensions.scala
+++ src/main/scala/utils/extensions.scala
@@ -10,19 +10,19 @@ object extensions {
     def highlighted(implicit formattingContext: FormattingContext): String =
       formattingContext match {
         case BashShell => s"\033[1m$original\033[0m"
-        case Html => s""""<span style="font-weight: bold;">$original</span>"""
+        case Html => s"""<span style="font-weight: bold;">$original</span>"""
       }
 
     def red(implicit formattingContext: FormattingContext): String =
       formattingContext match {
         case BashShell => s"\033[31m$original\033[0m"
-        case Html => s""""<span style="color: #ff0033;">$original</span>"""
+        case Html => s"""<span style="color: #ff0033;">$original</span>"""
       }
 
     def green(implicit formattingContext: FormattingContext): String =
       formattingContext match {
         case BashShell => s"\033[32m$original\033[0m"
-        case Html => s""""<span style="color: #42d647;">$original</span>"""
+        case Html => s"""<span style="color: #42d647;">$original</span>"""
       }
   }
 }
