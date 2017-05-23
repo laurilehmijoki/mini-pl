0128ba66bf949c9638553e3d1e311b2e7f875b7c
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2017-05-23 13:05:17 +0300

    test: expression

--- src/test/scala/frontend/VerifiedProgramSpec.scala
+++ src/test/scala/frontend/VerifiedProgramSpec.scala
@@ -46,6 +46,16 @@ class VerifiedProgramSpec extends Specification {
       {
         case Left(error +: Nil) => error.getClass must equalTo(classOf[IncompatibleTypes])
       }: PartialFunction[VerificationResult, MatchResult[_]]
+    ),
+    (
+      """
+        |var z : int := 1 + 2 * 3 * 4;
+        |var foo : int := 1 + z;
+        |print foo;
+        |""".stripMargin,
+      {
+        case Right(verifiedProgram) => verifiedProgram.statements must haveLength(3)
+      }: PartialFunction[VerificationResult, MatchResult[_]]
     )
   ) foreach Function.tupled { (program, x) =>
     s"verifier" should {
